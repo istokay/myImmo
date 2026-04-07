98fa01fe96411de1b4cec8a3687abf1acd92c736
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-04-07 12:00:45 +0200

    Backend - More models

--- MyImmo/MyImmo/App/Dtos/IncomeCategory.cs
+++ MyImmo/MyImmo/App/Dtos/IncomeCategory.cs
@@ -1,3 +1,5 @@
+namespace MyImmo.App.Dtos;
+
 public enum IncomeCategory
 {
     OneTimePayment,
---
+++ MyImmo/MyImmo/App/Dtos/Incomes.cs
@@ -0,0 +1,7 @@
+namespace MyImmo.App.Dtos;
+
+public class Incomes
+{
+    public int Id { get; set; }
+    public List<Income> Income { get; set; }
+}
