dd1d17f6164e6c7d81041e044f7796626b04fbd7
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-04-17 10:21:20 +0200

    Components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RealEstateComponent } from '../components/real-estate-component/real-estate-component';
+import { HomeComponent } from '../components/home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'immos',
+        component: RealEstateComponent
+    },
+    {
+        path: '',
+        component: HomeComponent
+    }, 
+    {
+        path: '**',
+        component: HomeComponent
+    }
+];
