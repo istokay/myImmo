b315c82fa06ce1cf42dd58b991212e6c38fe4b4e
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-04-30 09:55:06 +0200

    Save

--- MyImmo/MyImmo/Infrastructure/Repositories/RealEstateRepository.cs
+++ MyImmo/MyImmo/Infrastructure/Repositories/RealEstateRepository.cs
@@ -24,51 +24,6 @@ public class RealEstateRepository(RealEstateDbContext dbContext) : IRealEstateRe
         };
         return result;
     }
-
-    public async Task<Income?> CreateIncome(int realEstateId, IncomePost incomePost)
-    {
-        var realEstate = await dbContext.RealEstates.FirstOrDefaultAsync(re => re.Id == realEstateId);
-
-        if (realEstate == null)
-            return null;
-
-        var entity = dbContext.Add(new IncomeEntity
-        {
-            Name = incomePost.Name,
-            Amount = incomePost.Amount,
-            Category = incomePost.IncomeCategory,
-            RealEstateId = realEstateId,
-        });
-
-        await dbContext.SaveChangesAsync();
-
-        return new Income
-        {
-            Id = entity.Entity.Id,
-            Name = entity.Entity.Name,
-            Amount = entity.Entity.Amount,
-            RealEstateId = entity.Entity.RealEstateId,
-            IncomeCategory = entity.Entity.Category
-        };
-    }
-
-    public async Task<bool> DeleteRealEstateIncome(int realEstateId, int incomeId)
-    {
-        var isDeleted = true;
-
-        var income = await dbContext.Incomes.FirstOrDefaultAsync(i => i.Id == realEstateId && i.RealEstateId == realEstateId);
-
-        if (income == null)
-        {
-            return !isDeleted;
-        }
-        dbContext.Incomes.Remove(income);
-
-        await dbContext.SaveChangesAsync();
-
-        return isDeleted;
-    }
-
     public async Task<IReadOnlyCollection<RealEstate>> GetAllRealEstates()
     {
         var result = await dbContext.RealEstates.Select(entitie => new RealEstate
@@ -78,25 +33,6 @@ public class RealEstateRepository(RealEstateDbContext dbContext) : IRealEstateRe
         }).ToArrayAsync();
 
         return result;
-    }
-
-    public async Task<IReadOnlyCollection<Income>?> GetImcomes(int realEstateId)
-    {
-        var realEstate = await dbContext.RealEstates.FirstOrDefaultAsync(re => re.Id == realEstateId);
-
-        if (realEstate == null)
-            return null;
-
-        var entities = await dbContext.Incomes.Where(i => i.RealEstateId == realEstateId).Select(i => new Income
-        {
-            Id = i.Id,
-            Name = i.Name,
-            Amount = i.Amount,
-            IncomeCategory = i.Category,
-            RealEstateId = i.RealEstateId
-        }).ToListAsync();
-
-        return entities;
     }
 
     public async Task<RealEstate?> UpdateRealEstate(int id, RealEstatePost realEstate)
@@ -112,31 +48,6 @@ public class RealEstateRepository(RealEstateDbContext dbContext) : IRealEstateRe
         {
             Id = entity.Id,
             Name = entity.Name,
-        };
-    }
-
-
-    public async Task<Income?> UpdateIncome(int realEstateId, int incomeId, IncomePost incomePost)
-    {
-        var entity = await dbContext.Incomes.FirstOrDefaultAsync(i => i.Id == incomeId && i.RealEstateId == realEstateId);
-
-        if (entity == null || entity.RealEstate?.Name == null || entity.RealEstate?.Id == null)
-        {
-            return null;
-        }
-
-        return new Income
-        {
-            Id = entity.Id,
-            Name = entity.Name,
-            Amount = entity.Amount,
-            IncomeCategory = entity.Category,
-            RealEstate = new RealEstate
-            {
-                Name = entity.RealEstate.Name,
-                Id = entity.RealEstate.Id
-            },
-            RealEstateId = entity.RealEstateId
         };
     }
 
