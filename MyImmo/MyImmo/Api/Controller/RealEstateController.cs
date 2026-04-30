2c76d30f261e28dbbcedddcd87b27ecd835f82ae
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-04-30 09:43:33 +0200

    Backend - split controller into income and real estate

--- MyImmo/MyImmo/Api/Controller/RealEstateController.cs
+++ MyImmo/MyImmo/Api/Controller/RealEstateController.cs
@@ -17,24 +17,6 @@ public class RealEstateController(IRealEstateService realEstateService) : Contro
         return Ok(response);
     }
 
-    [HttpPost("{realEstateId}/income")]
-    public async Task<ActionResult<RealEstateResponseDto>> CreateRealEstateIncome(int realEstateId, [FromBody] IncomePost incomePost)
-    {
-        try
-        {
-            var response = await realEstateService.CreateIncome(realEstateId, incomePost);
-            return Ok(response);
-        }
-        catch (EntityNotFoundException)
-        {
-            return NotFound();
-        }
-        catch (Exception)
-        {
-            return StatusCode(500);
-        }
-    }
-
     [HttpGet]
     public async Task<ActionResult<AllRealEstatesResponseDto>> GetAllRealEstates()
     {
@@ -44,42 +26,6 @@ public class RealEstateController(IRealEstateService realEstateService) : Contro
             return Ok(response);
         else
             return NotFound();
-    }
-
-    [HttpGet("{id}")]
-    public async Task<ActionResult<RealEstateResponseDto>> GetRealEstateIncomes(int id)
-    {
-        try
-        {
-            var response = await realEstateService.GetImcomes(id);
-            return Ok(response);
-        }
-        catch (EntityNotFoundException)
-        {
-            return NotFound();
-        }
-        catch (Exception)
-        {
-            return StatusCode(500);
-        }
-    }
-
-    [HttpDelete("{realEstateId}/{incomeId}")]
-    public async Task<ActionResult> DeleteIncomeById(int realEstateId, int incomeId)
-    {
-        try
-        {
-            await realEstateService.DeleteRealEstateIncome(realEstateId, incomeId);
-            return Ok();
-        }
-        catch (EntityNotFoundException)
-        {
-            return NotFound();
-        }
-        catch (Exception)
-        {
-            return StatusCode(500);
-        }
     }
 
     [HttpDelete("{realEstateId}")]
@@ -106,23 +52,6 @@ public class RealEstateController(IRealEstateService realEstateService) : Contro
         {
             var result = await realEstateService.UpdateRealEstate(id, realEstate);
             return result;
-        }
-        catch (EntityNotFoundException)
-        {
-            return NotFound();
-        }
-        catch (Exception)
-        {
-            return StatusCode(500);
-        }
-    }
-    [HttpPut("{realEstateId}/{incomeId}")]
-    public async Task<ActionResult<Income>> UpdateRealEstateIncome(int realEstateId, int incomeId, [FromBody] IncomePost income)
-    {
-        try
-        {
-            var result = await realEstateService.UpdateIncome(realEstateId, incomeId, income);
-            return Ok(result);
         }
         catch (EntityNotFoundException)
         {
