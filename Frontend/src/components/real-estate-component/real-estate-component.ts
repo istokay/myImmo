c4254b639e2e6a88d5ab62f7b63c1c658e737bf2
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-04-30 13:54:37 +0200

    Frontend - router outlet layout

--- Frontend/src/components/real-estate-component/real-estate-component.ts
+++ Frontend/src/components/real-estate-component/real-estate-component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RealEstateService } from './services/real-estate-service';
 import { RealEstateApiService } from './api/services/real-estate-api-service';
 import { AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-real-estate-component',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatCardModule, MatButtonModule],
   templateUrl: './real-estate-component.html',
   styleUrl: './real-estate-component.css',
   providers: [RealEstateService, RealEstateApiService],
