3ff590cdf9bd558b7e654d099d9f344e42a8d189
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-05-03 11:48:27 +0200

    Frontend - display real estates in table

--- Frontend/src/components/real-estate-component/real-estate-component.ts
+++ Frontend/src/components/real-estate-component/real-estate-component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, model, ModelSignal } from '@angular/core';
-import { RealEstateService } from './services/real-estate-service';
+import { Component, computed, inject, model, ModelSignal } from '@angular/core';
 import { RealEstateApiService } from './api/services/real-estate-api-service';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,10 +6,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { RealEstateDialogComponent } from './dialog-component/dialog-component';
 import { RealEstate } from './api/dtos/realEstate';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-real-estate-component',
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './real-estate-component.html',
   styleUrl: './real-estate-component.css',
   providers: [],
@@ -19,6 +19,9 @@ export class RealEstateComponent {
   readonly realEstate: ModelSignal<RealEstate | undefined> = model();
   readonly dialog = inject(MatDialog);
   readonly realEstateApiService = inject(RealEstateApiService);
+
+  displayedColumns: string[] = ['name'];
+  dataSource = computed(() => this.realEstateApiService.realEstates());
   openDialog(): void {
     const dialogRef = this.dialog.open(RealEstateDialogComponent, {
       data: this.realEstateApiService,
