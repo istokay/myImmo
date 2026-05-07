81fa6ddef84737bdea2d69cc5c383658e46a675f
Tanja Jurjut <tanjajurjut@Mac.fritz.box>
2026-05-07 13:53:58 +0200

    Backend - remove unused dtos

--- MyImmo/MyImmo/Api/Dtos/CreateRealEstateRequestDto.cs
+++
@@ -1,9 +0,0 @@
-using MyImmo.Domain.Dtos;
-
-namespace MyImmo.Api.Dtos;
-
-public class CreateRealEstateRequestDto
-{
-    public required string Name { get; set; }
-    public List<IncomePost>? Incomes { get; set; }
-}
--- MyImmo/MyImmo/Api/Dtos/GetRealEstateRequestDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace MyImmo.Api.Dtos;
-
-public class GetRealEstateRequestDto
-{
-    [NotNull]
-    public required int Id { get; set; }
-}
